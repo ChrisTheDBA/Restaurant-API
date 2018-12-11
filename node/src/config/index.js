fc88882a8742e5d5f85a282bfa7a33db294c8403
Chris Mathews <christhedba@gmail.com>
2018-12-11 05:42:01 -0500

    First commit

---
+++ node/src/config/index.js
@@ -0,0 +1,4 @@
+export default {
+	"port": 3000,
+	"mongoURL": "mongodb://db:27017/restaurant-api"
+}
---
+++ node/src/routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import config from '../config';
+import middleware from '../middleware';
+import initializeDB from '../db';
+
+//connect to db
+
+
+
+
